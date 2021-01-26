53902ff715e25ed35d0b91e807a7ef850431883a
Dario <darodangelo@gmail.com>
2021-01-26 17:25:16 -0300

    Se agrega el servicio Autenticacion con los metodos login y logout

---
+++ src/app/servicios/autenticacion.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AutenticacionService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<Usuario>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+      return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>('http://localhost:8080/foodtruckme-spring/autenticacion', { username, password })
+        .pipe(map(credentials => {
+            if (credentials && credentials.token) {
+                localStorage.setItem('currentUser', JSON.stringify(credentials));
+                this.currentUserSubject.next(credentials);
+            }
+            return credentials;
+        }));
+}
+
+  logout() {
+      localStorage.removeItem('currentUser');
+      this.currentUserSubject.next(null);
+  }
+
+}
